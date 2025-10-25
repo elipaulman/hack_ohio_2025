c31ca813bfb87b41975a369d8fbb9648c0cc8917
Elijah Paulman <eli.paulman@gmail.com>
2025-10-25 14:31:03 -0400

    Update building rotation offset value
    Changed the buildingRotationOffset from 13 to 123 in IndoorNavigatorApp to better align compass readings with the floor plan orientation.

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ class IndoorNavigatorApp {
         // Building orientation offset
         // The floor plan's "north" (up on screen) corresponds to 77° on the compass
         // Subtract this offset from compass readings to align with the floor plan
-        this.buildingRotationOffset = 13;
+        this.buildingRotationOffset = 123;
 
         // App state
         this.isTracking = false;
